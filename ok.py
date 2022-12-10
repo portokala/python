91b599853752c24af500468a546f872d25f938ae
portokala <63567277+portokala@users.noreply.github.com>
2022-12-10 20:56:03 +0200

    Delete ok.py

--- ok.py
+++
@@ -1,8 +0,0 @@
-name = input("What is your name macho?\n")
-print ("Ce faci fa " + (name) + "?\n Hai sa vedem daca stii sa calculezi pe degete")
-raspuns = int(input("cat face unu plus unu? "))
-raspuns_corect = 2
-if raspuns == raspuns_corect:
-    print("bravo boss hai ca nu esti asa prost")
-elif raspuns != raspuns_corect:
-    print("vai de capul tau")
